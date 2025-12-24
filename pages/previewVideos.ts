79f3f071ce5512d31d27b28f8abdcdcd1b47de81
aayush3892 <aayush3737@duck.com>
2025-12-24 22:00:57 +0000

    feat: update preview video list and enhance iframe security

---
+++ pages/previewVideos.ts
@@ -0,0 +1,87 @@
+export const PREVIEW_VIDEOS = [
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 05  LCM and HCF By Gagan sir [2025-12-22]",
+    "id": "1149238323",
+    "url": "https://vimeo.com/1149238323"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 01  LCM and HCF By Gagan sir [2025-12-14]",
+    "id": "1149238324",
+    "url": "https://vimeo.com/1149238324"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 04  LCM and HCF By Gagan sir [2025-12-19]",
+    "id": "1149238325",
+    "url": "https://vimeo.com/1149238325"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 02  LCM and HCF By Gagan sir [2025-12-16]",
+    "id": "1149238329",
+    "url": "https://vimeo.com/1149238329"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 03  LCM and HCF By Gagan sir [2025-12-16]",
+    "id": "1149238330",
+    "url": "https://vimeo.com/1149238330"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 08  Surds and Indices By Gagan sir [2025-12-10]",
+    "id": "1149238504",
+    "url": "https://vimeo.com/1149238504"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 01  Surds and Indices By Gagan sir [2025-12-01]",
+    "id": "1149238506",
+    "url": "https://vimeo.com/1149238506"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 09  Surds and Indices By Gagan sir [2025-12-10]",
+    "id": "1149238503",
+    "url": "https://vimeo.com/1149238503"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 02  Surds and Indices By Gagan sir [2025-12-02]",
+    "id": "1149238505",
+    "url": "https://vimeo.com/1149238505"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 10  Surds and Indices By Gagan sir [2025-12-12]",
+    "id": "1149238507",
+    "url": "https://vimeo.com/1149238507"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 05  Surds and Indices By Gagan sir [2025-12-05]",
+    "id": "1149238674",
+    "url": "https://vimeo.com/1149238674"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 04  Surds and Indices By Gagan sir [2025-12-03]",
+    "id": "1149238673",
+    "url": "https://vimeo.com/1149238673"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 06  Surds and Indices By Gagan sir [2025-12-08]",
+    "id": "1149238675",
+    "url": "https://vimeo.com/1149238675"
+  },
+  {
+    "title": "𝖳𝗂𝗍𝗅𝖾 : (Advance)  Class - 03  Surds and Indices By Gagan sir [2025-12-03]",
+    "id": "1149238676",
+    "url": "https://vimeo.com/1149238676"
+  },
+  {
+    "title": "ADVANCE",
+    "id": "1149238921",
+    "url": "https://vimeo.com/1149238921"
+  },
+  {
+    "title": "ADVANCE",
+    "id": "1149238922",
+    "url": "https://vimeo.com/1149238922"
+  },
+  {
+    "title": "ADVANCE",
+    "id": "1149238923",
+    "url": "https://vimeo.com/1149238923"
+  }
+];
